5906d152e69248e06e3dc51d22e685e3d79ae7a5
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-01-21 19:44:10 +0000

    Add function to load next question.

--- js/logic.js
+++ js/logic.js
@@ -4,27 +4,71 @@ const questionsDiv = document.getElementById("questions");
 const questionTitle = document.getElementById("question-title");
 const choicesList = document.getElementById("choices-list");
 
+const questionsNumber = 2;
+let currentQuestion = 0;
+let questionAnswered = false;
+
 function startQuiz() {
   startScreenDiv.classList.add("hide");
-  questionsDiv.classList.add("start");
   questionsDiv.classList.remove("hide");
 }
 
-function game() {
-  questionTitle.textContent = questions[0].question;
-
+function clearCurrentQuestion() {
+  questionTitle.textContent = "";
   choicesList.innerHTML = "";
-  questions[0].choices.forEach(function (choice) {
-    let choiceButton = document.createElement("button");
-    choiceButton.innerHTML = choice;
+}
 
-    choicesList.appendChild(choiceButton);
+function choiceCheck(btn) {
+  btn.addEventListener("click", function (e) {
+    if (questionAnswered) {
+      return;
+    }
+
+    questionAnswered = true;
+
+    e.preventDefault();
+
+    questionTitle.classList.add("responded");
+
+    let message;
+    if (btn.textContent === questions[currentQuestion].correctChoice) {
+      message = "Correct!";
+    } else {
+      message = "Wrong!";
+    }
+
+    const response = document.createElement("h3");
+    response.textContent = message;
+    choicesList.appendChild(response);
+
+    let nextButton = document.createElement("button");
+    nextButton.textContent = "NEXT QUESTION";
+    choicesList.appendChild(nextButton);
+    nextButton.addEventListener("click", nextQuestion);
   });
 }
 
-startButton.addEventListener("click", startQuiz, {
-  passive: true,
-  once: true,
-});
+function renderQuestion() {
+  questionTitle.textContent = questions[currentQuestion].question;
 
-game();
+  for (let j = 0; j < questions[currentQuestion].choices.length; j++) {
+    let choiceButton = "";
+
+    choiceButton = document.createElement("button");
+    choiceButton.innerHTML = questions[currentQuestion].choices[j];
+    choicesList.appendChild(choiceButton);
+
+    choiceCheck(choiceButton);
+  }
+}
+
+function nextQuestion() {
+  clearCurrentQuestion();
+  currentQuestion = currentQuestion + 1;
+  renderQuestion();
+  questionAnswered = false;
+}
+
+startButton.addEventListener("click", startQuiz);
+
+renderQuestion();
