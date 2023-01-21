a147b8bb98fe4def224eb6b9f00f8a304cc72e14
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-01-21 20:21:59 +0000

    Add question.

--- js/questions.js
+++ js/questions.js
@@ -3,13 +3,25 @@ const questions = [
     index: 0,
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["javascript", "script", "js", "scripting"],
-    correctChoice: 2,
+    correctChoice: "script",
   },
   {
     index: 1,
     question: "Where is the correct place to insert a JavaScript?",
     choices: ["head", "body", "both"],
-    correctChoice: 3,
+    correctChoice: "both",
+  },
+  {
+    index: 2,
+    question:
+      "Which of the following will write the message “Hello World!” in an alert box?",
+    choices: [
+      "alertBox (“Hello World”);",
+      "alert (Hello World”!);",
+      "msgAlert (“Hello World”!”);",
+      "alert (“Hello World”!”);",
+    ],
+    correctChoice: "alert (“Hello World”!”);",
   },
 ];
 
