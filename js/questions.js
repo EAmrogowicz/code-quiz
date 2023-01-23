814b27d8c8c303510e59aca8b873d691720f710c
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-01-23 15:17:18 +0000

    Add more questions to array.

--- js/questions.js
+++ js/questions.js
@@ -1,20 +1,17 @@
 const questions = [
   {
-    index: 0,
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["javascript", "script", "js", "scripting"],
     correctChoice: "script",
   },
   {
-    index: 1,
     question: "Where is the correct place to insert a JavaScript?",
     choices: ["head", "body", "both"],
     correctChoice: "both",
   },
   {
-    index: 2,
     question:
-      "Which of the following will write the message “Hello World!” in an alert box?",
+      "Which of the following will write the message -Hello World!- in an alert box?",
     choices: [
       "alertBox (“Hello World”);",
       "alert (Hello World”!);",
@@ -22,6 +19,59 @@ const questions = [
       "alert (“Hello World”!”);",
     ],
     correctChoice: "alert (“Hello World”!”);",
+  },
+  {
+    question: "Which of the following is correct about JavaScript?",
+    choices: [
+      "JavaScript is an Object-Based language",
+      "JavaScript is Assembly-language",
+      "JavaScript is an Object-Oriented language",
+      "JavaScript is a High-level language",
+    ],
+    correctChoice: "JavaScript is an Object-Based language",
+  },
+  {
+    question:
+      "Arrays in JavaScript are defined by which of the following statements?",
+    choices: [
+      "It is an ordered list of objects",
+      "It is an ordered list of values",
+      "It is an ordered list of string",
+      "It is an ordered list of functions",
+    ],
+    correctChoice: "It is an ordered list of values",
+  },
+  {
+    question: "Why event handlers is needed in JS?",
+    choices: [
+      "Adds innerHTML page to the code",
+      "Change the server location",
+      "Performs handling of exceptions and occurrences",
+      "Allows JavaScript code to alter the behaviour of windows",
+    ],
+    correctChoice: "Allows JavaScript code to alter the behaviour of windows",
+  },
+  {
+    question:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    choices: [
+      "getElementbyId()",
+      "getElementByClassname()",
+      "Both above",
+      "None of the above",
+    ],
+    correctChoice: "Both above",
+  },
+  {
+    question:
+      "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    choices: [
+      "Throws an error",
+      "Ignores the statement",
+      "Gives a warning",
+      "None of the above",
+    ],
+    correctChoice: "Ignores the statement",
   },
 ];
 
